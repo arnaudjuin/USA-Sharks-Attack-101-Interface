69281438d87fc8ed2102763bc2c984872da58f8c
arnaudjuin <arnaud.juin@gmail.com>
2020-10-28 10:09:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,145 +1,78 @@
 import React, { Component } from "react";
 import {
+  ReactiveList,
   ReactiveBase,
   DataSearch,
   NumberBox,
+  RangeInput,
   DateRange,
-  RangeSlider
+  SingleList,
+  RangeSlider,
 } from "@appbaseio/reactivesearch";
 import { ReactiveGoogleMap } from "@appbaseio/reactivemaps";
 import "./App.css";
 
 class App extends Component {
-  onPopoverClick = function(data) {
-    return (
-      <div className="popover">
-        <div className="image-container">
-          <img src={data.image} alt={data.name} height="185" width="263" />
-        </div>
-        <div className="extra-info-container">
-          <div className="type-container info">
-            {data.room_type}-{data.beds} bed
-          </div>
-          <div className="name-container info">{data.name}</div>
-          <div className="price-container info">
-            ${data.price} per night-Free cancellation
-          </div>
-        </div>
-      </div>
-    );
-  };
   render() {
     return (
       <div className="main-container">
         <ReactiveBase
-          app="airbeds-test-app"
-          credentials="X8RsOu0Lp:9b4fe1a4-58c6-4089-a042-505d86d9da30"
-          type="listing"
+          app="rettetetetettet"
+          credentials="NADMdVx0I:f5579985-2e52-4f5f-bcaf-a47452cac311"
           theme={{
             colors: {
-              primaryColor: "#41ABF5"
-            }
+              primaryColor: "#41ABF5",
+            },
           }}
         >
-          <div className="nav-container">
-            <nav className="nav">
-              <div className="title">Airbeds</div>
-            </nav>
+          <div className="search-container">
+            <DataSearch
+              componentId="search"
+              dataField="Name"
+              placeholder="Search   names..."
+              iconPosition="left"
+              className="search"
+            />
+            <RangeInput
+  dataField="Age"
+  componentId="AgeRange"
+  range={{
+    start: 0,
+    end: 100,
+  }}
+  rangeLabels={{
+    start: '0',
+    end: '100',
+  }}
+/>
+            <DateRange
+              componentId="SpeciesFilter"
+              dataField="Date"
+              title="DateRange"
+              placeholder={{
+                start: "Start Date",
+                end: "End Date",
+              }}
+              queryFormat="date"
+              autoFocusEnd={true}
+              showClear={true}
+              showFilter={true}
+              filterLabel="Date"
+              URLParams={false}
+            />{" "}
           </div>
-          <div className="filters-search-container">
-            <div className="filter-container">
-              <div className="dropdown">
-                <button className="button">Price</button>
-                <div className="dropdown-content">
-                  <RangeSlider
-                    componentId="PriceSensor"
-                    dataField="price"
-                    title="Price Range"
-                    range={{
-                      start: 10,
-                      end: 250
-                    }}
-                    rangeLabels={{
-                      start: "$10",
-                      end: "$250"
-                    }}
-                    defaultValue={{
-                      start: 10,
-                      end: 50
-                    }}
-                    stepValue={10}
-                    interval={20}
-                    react={{
-                      and: ["DateRangeSensor", "GuestSensor"]
-                    }}
-                    className="rangeFilter"
-                  />
-                </div>
-              </div>
-              <div className="dropdown">
-                <button className="button">Guests</button>
-                <div className="dropdown-content-guest">
-                  <NumberBox
-                    componentId="GuestSensor"
-                    dataField="accommodates"
-                    title="Guests"
-                    defaultValue={2}
-                    labelPosition="right"
-                    data={{
-                      start: 1,
-                      end: 16
-                    }}
-                    className="numberFilter"
-                  />
-                </div>
-              </div>
-
-              <div className="dropdown">
-                <button className="button ">When</button>
-                <div className="dropdown-content">
-                  <DateRange
-                    dataField="date_from"
-                    componentId="DateRangeSensor"
-                    title="When"
-                    numberOfMonths={2}
-                    queryFormat="basic_date"
-                    initialMonth={new Date("04/01/2017")}
-                    className="dateFilter"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="search-container">
-              <DataSearch
-                componentId="search"
-                dataField="name"
-                autosuggest={false}
-                placeholder="Search housings..."
-                iconPosition="left"
-                className="search"
-              />
-            </div>
-          </div>
-
           <div className="result-map-container">
             <ReactiveGoogleMap
               componentId="map"
+              defaultMapStyle="Flat Map"
               dataField="location"
-              defaultZoom={13}
+              defaultZoom={5}
               pagination
-              onPageChange={() => {
-                window.scrollTo(0, 0);
-              }}
-              style={{
-                width: "calc(100% - 280px)",
-                height: "calc(100vh - 52px)"
-              }}
-              onPopoverClick={this.onPopoverClick}
-              className="rightCol"
+              showMarkers={true}
               showMarkerClusters={false}
-              showSearchAsMove={false}
+              showSearchAsMove={true}
               innerClass={{
-                label: "label"
+                label: "label",
               }}
               renderAllData={(
                 hits,
@@ -148,54 +81,115 @@ class App extends Component {
                 renderMap,
                 renderPagination
               ) => (
-                <div style={{ display: "flex" }}>
-                  <div>
-                    <div className="card-container">
-                      {hits.map(data => (
-                        <div key={data._id} className="card">
-                          <div
-                            className="card__image"
-                            style={{ backgroundImage: `url(${data.image})` }}
-                            alt={data.name}
-                          />
-                          <div>
-                            <h2>{data.name}</h2>
-                            <div className="card__price">${data.price}</div>
-                            <p className="card__info">
-                              {data.room_type} · {data.accommodates} guests
-                            </p>
-                          </div>
+                <div>
+                  <div className="flex-container">
+                    <div className="d-none  d-lg-flex card-container">
+                      {" "}
+                      {hits.map((data) => (
+                        <div
+                          key={data._id}
+                          className="card text-white bg-danger mb-3"
+                        >
+                          <div className="card-header">
+                            {" "}
+                            {data.Sex} - {data.Name} - {data.Age}  years{" "}
+                          </div>{" "}
+                          <div className="card-text white-card">
+                            <div
+                              data-toggle="tooltip"
+                              data-html="true"
+                              title="White Shark"
+                              data-placement="top"
+                              className="card__image"
+                              style={{
+                                backgroundImage: `url( "${data.Image}" )`,
+                              }}
+                              alt={data.Species}
+                            />{" "}
+                            <div className="pt-2 attack-data">
+                            <div className="activity">Activity : {data.Activity}</div>
+                            <div className="type">Type : {data.Type}</div>
+                              <a
+                                target="_blank"
+                                href={data.href}
+                                className="mt-2 card__info"
+                              >
+                                <svg
+                                  width="1.5em"
+                                  height="1.5em"
+                                  viewBox="0 0 16 16"
+                                  className="mr-2 bi bi-file-earmark-text"
+                                  fill="currentColor"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
+                                  <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z" />
+                                  <path
+                                    fillRule="evenodd"
+                                    d="M5 11.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                                  />
+                                </svg>
+                                {data.Location} · {data.Date}{" "}
+                              </a>{" "}
+                            </div>{" "}
+                          </div>{" "}
                         </div>
-                      ))}
-                    </div>
-                    <div>{renderPagination()}</div>
-                  </div>
-                  <div className="map-container">{renderMap()}</div>
+                      ))}{" "}
+                      <div> {renderPagination()} </div>{" "}
+                    </div>{" "}
+                    <div className="map-container"> {renderMap()} </div>{" "}
+                  </div>{" "}
                 </div>
               )}
-              renderData={data => ({
+              renderData={(data) => ({
                 label: (
                   <div
                     className="marker"
                     style={{
-                      width: 40,
-                      display: "block",
-                      textAlign: "center"
                     }}
                   >
-                    <div className="extra-info">{data.name}</div>${data.price}
+                    <div className="extra-info"> </div> {data.Name} -{" "}
+                    {data.Date}{" "}
                   </div>
-                )
+                ),
               })}
               react={{
-                and: ["GuestSensor", "PriceSensor", "DateRangeSensor", "search"]
+                and: ["search", "DateSensor", "SpeciesFilter","AgeRange"],
               }}
-            />
-          </div>
-        </ReactiveBase>
+            />{" "}
+          </div>{" "}
+        </ReactiveBase>{" "}
       </div>
     );
   }
 }
 
 export default App;
+
+// TO DO
+
+//RELOAD
+//responsive
+//MARKERS
+//FATAL
+//PUSH
+
+//   <div className="row">
+//             <div className="col">
+//               <SingleList
+//                 title="Places"
+//                 componentId="SpeciesFilter"
+//                 dataField="Species.keyword"
+//                 size={50}
+//                 showSearch
+//               />
+//               <div
+//                 style={{
+//                   marginTop: "20px",
+//                   marginBottom: "5px",
+//                   fontSize: "1 rem"
+//                 }}
+//               >
+//               </div>
+// </div>
+// </div>
